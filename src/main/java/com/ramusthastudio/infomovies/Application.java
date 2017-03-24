2b8f6f36d55ef4193c391bf94487335f9ad69108
ramustha <ramusthanest@gmail.com>
2017-03-24 09:58:13 +0000

    update

--- src/main/java/com/ramusthastudio/infomovies/Application.java
+++ src/main/java/com/ramusthastudio/infomovies/Application.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+// @EnableScheduling
 public class Application extends SpringBootServletInitializer {
   @Override
   protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
